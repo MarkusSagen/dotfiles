630480d02eba5c73cd22340d5bb44b8206bced16
MarkusSagen <markus.john.sagen@gmail.com>
2020-03-29 19:33:30 +0200

    Updated dotfiles

---
+++ save_Arch_configs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Copy files from local changes and push to Github
+cp ~/.zshrc Arch
+cp ~/.vimrc Arch
+cp ~/.tmux.conf Arch
+cp ~/install.sh Arch
+
+git status
+git add -A
+git commit -m "Updated dotfiles"
+git push
+
+
