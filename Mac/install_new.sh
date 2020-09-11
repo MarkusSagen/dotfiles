5924e3f211c887835b157dabce6788f497310cfa
Markus Sagen <Markus.John.Sagen@gmail.com>
2020-09-11 11:30:26 +0200

    Create install_new.sh

---
+++ Mac/install_new.sh
@@ -0,0 +1,21 @@
+
+
+brew install sqlite3
+echo 'export PATH="/usr/local/opt/sqlite/bin:$PATH"' >> ~/.profile
+brew install fzf
+brew install python3
+brew install vim
+brew install neovim
+brew install httpie
+brew install fzy 									# Other fuzzy finder
+brew install tmuxinator 					# Other terminal
+brew install the_silver_searcher 	# Fast search with 'ag'
+brew install fzf 									# Fuzzy finder, type '**<TAB>' or just <TAB> to search
+brew install ripgrep
+brew install cmake
+brew install cask mactex
+
+# Install powerline and other fonts
+pip3 install powerline-status
+git clone https://github.com/edwardtufte/et-book.git ~/
+cd ~/et-book/et-book/~/et-book/et-book/et-book-semi-bold-old-style-figures /
