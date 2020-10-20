75405ed9e4ddf41c3a7b34bef5dd0061b3e799f7
Markus Sagen <markus.sagen@peltarion.com>
2020-10-20 17:00:53 +0200

    Minimal config + Emacs TRAMP config

--- minimal/install.sh
+++ minimal/install.sh
@@ -1,6 +1,9 @@
 
-
-
+# Switch shell to zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+zsh
+export SHELL=bin/zsh
+exec /bin/zsh -l
 
 # Download and install new configured TMUX
 git clone https://github.com/samoshkin/tmux-config.git ~/
@@ -19,11 +22,38 @@ git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:=~/.oh-my-zs
 # zsh-syntax-highlight
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# zsh configs
+
+# Add these settings
+echo "# For remote shell access
+if [[ "$TERM" == "dumb" ]]; then
+  unset zle_bracketed_paste
+  unset zle
+  PS1='$ '
+  return
+fi" >> .zshrc
+
+echo "# Completion and suggest commands when typing
+plugins=(git zsh-completions zsh-syntax-highlighting zsh-autosuggestions)" >> .zshrc
+
+echo "# Load shell changes
+source $ZSH/oh-my-zsh.sh" >> .zshrc
+
+# Set zsh as default shell when logging back in
+echo "export SHELL=bin/zsh" >> .bashrc
+echo "exec /bin/zsh -l" >> .bashrc
+
+# Reload and apply the settings
+source .zshrc
+source .bashrc
 
 # nvim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 cmod u+x nvim.appimage
 ./nvim.appimage
+
+mkdir ~/.vim/undodir -p
+
 
 # Doom emacs
 # git clone --depth 1 https://github.com/hlissner/doom-emacs ~/.emacs.d
